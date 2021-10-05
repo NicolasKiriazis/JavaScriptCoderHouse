73001bf5c0e773f120a9617fe31ccac5ce27ac5e
NicolasKiriazis <nicolaskiriazis@gmail.com>
2021-10-04 22:42:54 -0300

    clase1Kiriazis

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const ESPACIO = " ";
+const year = 2021;
+
+let nombre = prompt("Ingrese su Nombre:")
+let apellido = prompt("Ingrese su Apellido")
+
+
+console.log("Nombre:" + ESPACIO + nombre)
+console.log("Apellido:" + ESPACIO + apellido)
+
+
+alert("Bienvenido" + ESPACIO + nombre + ESPACIO + apellido)
+
+let born =  parseInt(prompt("¿En qué año naciste?")) 
+
+let age = year - born;
+
+alert("Tu edad es:" + ESPACIO + age + ESPACIO + "años.")
+
+console.log("Edad:" + ESPACIO + age)
