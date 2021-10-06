fcde6a32a29c968d2d72141dd0135d832965ff5b
NicolasKiriazis <nicolaskiriazis@gmail.com>
2021-10-05 21:46:26 -0300

    edit navbar y cart

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-const ESPACIO = " ";
-const year = 2021;
+// const ESPACIO = " ";
+// const year = 2021;
 
-let nombre = prompt("Ingrese su Nombre:")
-let apellido = prompt("Ingrese su Apellido")
-
-
-console.log("Nombre:" + ESPACIO + nombre)
-console.log("Apellido:" + ESPACIO + apellido)
+// let nombre = prompt("Ingrese su Nombre:")
+// let apellido = prompt("Ingrese su Apellido")
 
 
-alert("Bienvenido" + ESPACIO + nombre + ESPACIO + apellido)
+// console.log("Nombre:" + ESPACIO + nombre)
+// console.log("Apellido:" + ESPACIO + apellido)
 
-let born =  parseInt(prompt("¿En qué año naciste?")) 
 
-let age = year - born;
+// alert("Bienvenido" + ESPACIO + nombre + ESPACIO + apellido)
 
-alert("Tu edad es:" + ESPACIO + age + ESPACIO + "años.")
+// let born =  parseInt(prompt("¿En qué año naciste?")) 
 
-console.log("Edad:" + ESPACIO + age)
+// let age = year - born;
+
+// alert("Tu edad es:" + ESPACIO + age + ESPACIO + "años.")
+
+// console.log("Edad:" + ESPACIO + age)
