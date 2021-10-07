0f46e2c47517e5a17b39c113b5b5348cc40f8922
NicolasKiriazis <nicolaskiriazis@gmail.com>
2021-10-06 22:32:00 -0300

    clase2Kiriazis

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 
-// const ESPACIO = " ";
-// const year = 2021;
+//---CONSTANTES---//
 
-// let nombre = prompt("Ingrese su Nombre:")
-// let apellido = prompt("Ingrese su Apellido")
+const ESPACIO = " ";
+const year = 2021;
 
+//---PEDIR NOMBRE Y APELLIDO---//
 
-// console.log("Nombre:" + ESPACIO + nombre)
-// console.log("Apellido:" + ESPACIO + apellido)
+let nombre = prompt("Ingrese su Nombre:")
+let apellido = prompt("Ingrese su Apellido")
 
+//---GUARDA EN CONSOLA---//
 
-// alert("Bienvenido" + ESPACIO + nombre + ESPACIO + apellido)
+console.log("Nombre:" + ESPACIO + nombre)
+console.log("Apellido:" + ESPACIO + apellido)
 
-// let born =  parseInt(prompt("¿En qué año naciste?")) 
+//---ALERT BIENVENIDA---//
 
-// let age = year - born;
+alert("Bienvenido" + ESPACIO + nombre + ESPACIO + apellido)
 
-// alert("Tu edad es:" + ESPACIO + age + ESPACIO + "años.")
+//---PREGUNTAR AÑO DE NACIMIENTO---//
 
-// console.log("Edad:" + ESPACIO + age)
+let born = parseInt(prompt("¿En qué año naciste?"))
+
+//---CONDICIONAL 1986---//
+
+if (born == 1986) {
+    alert("EL DIEGOOOOOOOO")
+} else if (born != 1986) {
+    alert("No nacio en el 1986, no trajo la copa")
+}
+
+//---VARIABLE BORN---//
+
+let age = year - born; //---- resta year menos la fecha ingresada ----//
+
+alert("Tu edad es:" + ESPACIO + age + ESPACIO + "años.")
+
+if (age < 21) {
+    alert("Usted es Menor de 21 años")
+    console.log("¿Es mayor de edad?:" + ESPACIO + "No")
+} else if (age > 80) {
+    alert("Estás viejito, cuidado...")
+} else {
+    alert("Usted es todo un adulto")
+    console.log("¿Es mayor de edad?:" + ESPACIO + "Si")
+}
+
+console.log("Edad:" + ESPACIO + age)
